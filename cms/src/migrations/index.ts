c41c291e7521b3a51abdd5a76c01dcddb6fadb55
harshita <harshita.chaturvedi2002@gmail.com>
2026-06-29 09:51:16 +0530

    Remove old Payload migrations

--- cms/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260618_060013 from './20260618_060013';
-
-export const migrations = [
-  {
-    up: migration_20260618_060013.up,
-    down: migration_20260618_060013.down,
-    name: '20260618_060013'
-  },
-];
