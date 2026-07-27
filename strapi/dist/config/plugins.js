6789828c9b7f947978528fd6a25c96b48e03c6a4
Harshi115 <harshita.chaturvedi2002@gmail.com>
2026-07-27 13:29:20 +0530

    Allow Cloudinary images in security policy and fix plugins config

--- strapi/dist/config/plugins.js
+++ strapi/dist/config/plugins.js
@@ -1,4 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const config = ({ env }) => ({});
+const config = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('dzmj2j8rh'),
+                api_key: env('916956428227632'),
+                api_secret: env('1PBpquTU9efY_5yPqE6ljHUKSKk'),
+            },
+            actionOptions: {
+                upload: {},
+                delete: {},
+            },
+        },
+    },
+});
 exports.default = config;
