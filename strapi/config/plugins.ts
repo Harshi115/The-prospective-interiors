0a262809c726f3ccac178559e92a09c426545c05
Harshi115 <harshita.chaturvedi2002@gmail.com>
2026-07-27 15:15:17 +0530

    Fix source plugins.ts with correct env variable names

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,13 +1,12 @@
 import type { Core } from '@strapi/strapi';
-
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('dzmj2j8rh'),
-        api_key: env('916956428227632'),
-        api_secret: env('1PBpquTU9efY_5yPqE6ljHUKSKk'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
@@ -16,5 +15,4 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
     },
   },
 });
-
 export default config;
